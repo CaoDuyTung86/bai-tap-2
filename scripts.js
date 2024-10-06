ab37314ab5f352e9ac44947b452c9f4ce0e111ae
CaoDuyTung86 <149053559+CaoDuyTung86@users.noreply.github.com>
2024-10-06 22:16:52 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const togglePassword = document.querySelector('.toggle-password');
+const passwordField = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function () {
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+});
